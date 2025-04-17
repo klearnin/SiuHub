22358d9b84ce45f0fac6d3b4df82661a38b5542e
klearnin <2022300004072@whu.edu.cn>
2025-04-17 16:05:15 +0800

    修改部分bug

--- backend/router/auth.js
+++ backend/router/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controller/auth");
+const authController = require("../controllers/auth");
 const upload = require("../middleware/upload");
 const auth = require("../middleware/auth");
 
